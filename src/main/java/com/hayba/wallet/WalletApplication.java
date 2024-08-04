d2773e47255e13d6513be02b1139874ed196178c
Abass Adeyemi <abassadeyemi1@gmail.com>
2024-08-04 16:05:46 +0100

    first commit

---
+++ src/main/java/com/hayba/wallet/WalletApplication.java
@@ -0,0 +1,13 @@
+package com.hayba.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApplication.class, args);
+	}
+
+}
