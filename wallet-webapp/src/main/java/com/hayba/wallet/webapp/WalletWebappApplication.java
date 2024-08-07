b834680e3fee051f08832def1f44022cffde95b0
Abass Adeyemi <abassadeyemi1@gmail.com>
2024-08-07 22:28:15 +0100

    initial setup

---
+++ wallet-webapp/src/main/java/com/hayba/wallet/webapp/WalletWebappApplication.java
@@ -0,0 +1,11 @@
+package com.hayba.wallet.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletWebappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WalletWebappApplication.class, args);
+    }
+}
